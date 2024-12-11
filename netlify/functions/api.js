7f95d68505212d9befa021edfecab834d75b52f2
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2024-12-11 20:08:43 +0100

    allow  cors on all methods

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,9 +1,22 @@
 const BeautyHost = require('../../src/beautyHost')
 const OpenAiService = require('../../src/aiServices/openAiService');
 
+const CORS_HEADERS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Origin, Authorization, X-Requested-With, Content-Type, Accept',
+    'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS'
+}
+
 /* The `exports.handler` function is the entry point for the Netlify function. When the 
 function is triggered, this function will be executed. */
 exports.handler = async function (event, context) {
+    if (event.httpMethod == "OPTIONS") {
+        return {
+          statusCode: 200,
+          headers: CORS_HEADERS
+        };
+    }
+      
     // Parse the body of the event to retrieve the parameters that were passed to the function.
     const {
         openai_api_key,
@@ -19,6 +32,7 @@ exports.handler = async function (event, context) {
     // Return the results of the ai advice
     return {
         statusCode: 200,
+        ...CORS_HEADERS,
         body: advice,
     };
 };
--- netlify/functions/gift-finder.js
+++ netlify/functions/gift-finder.js
@@ -1,20 +1,21 @@
 const BeautyHost = require('../../src/beautyHost')
 const OpenAiService = require('../../src/aiServices/openAiService');
 
+const CORS_HEADERS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Origin, Authorization, X-Requested-With, Content-Type, Accept',
+    'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS'
+}
+
 /* The `exports.handler` function is the entry point for the Netlify function. When the 
 function is triggered, this function will be executed. */
 exports.handler = async function (event, context) {
     if (event.httpMethod == "OPTIONS") {
         return {
           statusCode: 200,
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Allow-Methods": "GET, POST, OPTION",
-          },
+          headers: CORS_HEADERS
         };
-      }
-
+    }
       
     // Parse the body of the event to retrieve the parameters that were passed to the function.
     const {
@@ -29,6 +30,7 @@ exports.handler = async function (event, context) {
     // Return the results of the ai advice
     return {
         statusCode: 200,
+        ...CORS_HEADERS,
         body: advice,
     };
 };
